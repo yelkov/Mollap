056fe87db4254170d1fdd6f04aa32ac7a52db494
yelkov <yelkovq@gmail.com>
2024-03-12 13:17:30 +0100

    build(mollap): creating repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mollap
+
+Solución al examen de programación Mollap de **Yelko Veiga** (1º DAM IES de Teis 23/24) a propuesta de @dfleta.
+
+Cuarto proyecto de aproximación a Java.
+
+## Examen programación Java
+
+
+
---
+++ app/src/main/java/edu/badpals/mollap/App.java
@@ -0,0 +1,31 @@
+
+package edu.badpals.mollap;
+
+public final class App {
+    public static void main(String[] args) {
+
+        /**
+         * Configuracion del programador de tareas
+         * del sistema con el tipo de target elegido:
+         * desde vehiculos a cualquier cosa que admita
+         * la recepcion de un mensaje.
+         */
+        ProgramadorTasques programadorTasques = new ProgramadorTasques(new Vehicle());
+
+        /**
+         * Añadir al sistema las tareas que queremos que el sistema
+         * ejecute al recibir la peticion del cliente.
+         */
+        programadorTasques.setTasca(new Autenticacio());
+        programadorTasques.setTasca(new Autoritzacio());
+
+        /**
+         * Configuracion de la app cliente para que
+         * ejecute las tareas programades y
+         * envie el mensaje al sistema.
+         */
+        Mollapp mollapp = new Mollapp();
+        mollapp.setProgramadorTasques(programadorTasques);
+        mollapp.enviarPeticio("Francesc");
+    }
+}
